4a37288facdd73a9c86f85aee2ebd8ce2a403e3b
Kazarin Fyodor <fekazar@gmail.com>
2023-09-17 15:04:29 +0300

    Linked write comment page with article id attribute

--- src/main/kotlin/edu/silly/blog/controller/CommentsController.kt
+++ src/main/kotlin/edu/silly/blog/controller/CommentsController.kt
@@ -1,6 +1,8 @@
 package edu.silly.blog.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -8,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam
 class CommentsController {
     @GetMapping("/write-comment")
     fun writeComment(
-        @RequestParam("article-id") articleId: Long
-    ) = "write-comment"
+        @RequestParam("article-id") articleId: Long,
+        model: Model,
+    ): String {
+        model["article_id"] = articleId
+        return "write-comment"
+    }
 }
